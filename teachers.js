d7cf0c3bcddb952de10c4d6a4159a36cfead5279
WebertiBarbosa <30930934+WebertiBarbosa@users.noreply.github.com>
2020-07-27 22:52:14 -0300

    add rotas e data.json

---
+++ teachers.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const data = require('./data.json')
+
+// create
+exports.post = function(req, res){
+
+    const keys = Object.keys(req.body)
+
+    for(key of keys){
+        if (req.body[key] == ""){
+            return res.send('Por favor, preencha todos os campos!')
+        }
+    }
+
+    let { avatar_url, birth, escolaridade, name, services } = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.teachers.length + 1)
+
+    
+
+    // [{...}]
+    data.teachers.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        escolaridade,
+        services,
+        created_at,
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) { 
+            return res.send('write file error!') 
+        
+        }
+        return res.redirect('teachers')
+
+    })
+
+    // return res.send(req.body)
+}
